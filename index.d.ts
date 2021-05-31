5c771ae06430b958dc8010ebf710d32fe18bd437
Touch <s.chantouch@mango-byte.com>
2021-05-31 22:47:23 +0700

    chore: :bug: fixed types

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,17 @@
-export default class ObjectTraversed {
-    get(path: string, defaultValue: any): string | object | any
-    has(path: string): boolean
-    set(path: string, value: any): any
-    push(path: string, value: any): void
-    create(path: string): void
-    delete(path: string): void
-    exec(path: string): any
-    isArray(path: string): boolean
-    isObject(path: string): boolean
-    isString(path: string): boolean
-    isNumber(path: string): boolean
+declare module 'object-traversed' {
+    export function traversed<T>(collection?: T[] | Object): Traversed;
+    export default function traversed<T>(collection?: T[] | Object): Traversed;
+    export class Traversed {
+        get(path: string, defaultValue: any): string | object | any
+        has(path: string): boolean
+        set(path: string, value: any): any
+        push(path: string, value: any): void
+        create(path: string): void
+        delete(path: string): void
+        exec(path: string): any
+        isArray(path: string): boolean
+        isObject(path: string): boolean
+        isString(path: string): boolean
+        isNumber(path: string): boolean
+    }
 }
